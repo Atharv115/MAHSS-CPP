d1671711bfb0f6d880db2e97a40511ebf96f8142
AtharvGolam <atharvgolam115@gmail.com>
2022-07-28 22:09:41 +0530

    removed.cpp instead inclued the working in header file

--- includes/fact.cpp
+++
@@ -1,9 +0,0 @@
-int fact(int x)
-{
-    int i, fact = 1;
-    for (i = x; i > 1; i--)
-    {
-        fact = fact * i;
-    }
-    return fact;
-}
--- includes/fact.h
+++ includes/fact.h
@@ -1,1 +1,9 @@
-int fact(int)
+int fact(int x)
+{
+    int i, fact = 1;
+    for (i = x; i > 1; i--)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
