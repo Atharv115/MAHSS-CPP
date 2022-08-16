8d5b8b35d13a95691c3eb1d4b9f85c207835e923
Atharv115 <atharvgolam115@gmail.com>
2022-08-16 19:38:55 +0530

    modified a lil bit as per requirements of journal

--- rev_integer.cpp
+++ rev_integer.cpp
@@ -5,28 +5,27 @@ using namespace std;
 class reverse
 {
 public:
-    int n, rev = 0;
+    int m, rev = 0;
     void input();
     void display();
 };
 void reverse::input()
 {
-    cout << "No Ghal:";
-    cin >> n;
-    while (n != 0)
+    cout << "Enter the number: ";
+    cin >> m;
+    while (m != 0)
     {
-        rev = rev * 10 + (n % 10);
-        n = n / 10;
+        rev = rev * 10 + (m % 10);
+        m = m / 10;
     }
 }
 void reverse::display()
 {
-    cout << rev;
+    cout << "The reverse of " << m << " is " << rev;
 }
 int main()
 {
     reverse r;
     r.input();
-    cout << "the reversed number is";
     r.display();
 }
