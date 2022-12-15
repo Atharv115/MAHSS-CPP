fed7c19487d307aace8349c827068de8b07dcaf5
Atharv115 <atharvgolam115@gmail.com>
2022-12-15 14:28:42 +0530

    sdgdfgdf

--- factorial.cpp
+++ factorial.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int i, n, fact = 1;
-    cout << "Enter the Number : ";
-    cin >> n;
-    for (i = n; i > 1; i--)
-    {
-        fact = fact * i;
-    }
+  int i, n;
+  double fact = 1;
+  cout << "Enter the Number : ";
+  cin >> n;
+  for (i = n; i > 1; i--) {
+    fact = fact * i;
+  }
     cout << fact;
 }
